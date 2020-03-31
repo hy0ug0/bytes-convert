7fda8d62e0acb1a22823e7ccda8212166c2a0cf2
Y0ug0 <hugo.contreras.pro@gmail.com>
2020-03-31 14:55:38 +0200

    Update release config

--- release.config.js
+++ release.config.js
@@ -2,8 +2,6 @@ module.exports = {
     branch: 'master',
     repositoryUrl: 'git@github.com:hy0ug0/bytes-convert.git',
     verifyConditions: ['@semantic-release/npm'],
-    prepare: true,
-    publish: true,
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
