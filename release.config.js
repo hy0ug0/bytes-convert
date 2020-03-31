978cbf1f10f8751b31b0381321ba7cef313c8c26
Y0ug0 <hugo.contreras.pro@gmail.com>
2020-03-31 16:22:45 +0200

    ci(release): update release config to release on ci change

--- release.config.js
+++ release.config.js
@@ -3,7 +3,16 @@ module.exports = {
     repositoryUrl: 'git@github.com:hy0ug0/bytes-convert.git',
     verifyConditions: ['@semantic-release/npm'],
     plugins: [
-        '@semantic-release/commit-analyzer',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    { type: 'docs', scope: 'README', release: 'patch' },
+                    { type: 'refactor', release: 'patch' },
+                    { type: 'ci', release: 'patch' },
+                ],
+            },
+        ],
         '@semantic-release/release-notes-generator',
         [
             '@semantic-release/npm',
